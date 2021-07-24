6310050315587cfaada1a462db1623498e13bff2
Rudolf Hersén <rudolf.hersen@schibsted.com>
2021-07-24 12:19:05 +0200

    scale images by devicePixelRatio

--- src/Tweet.jsx
+++ src/Tweet.jsx
@@ -52,7 +52,11 @@ export default function Tweet(props) {
           <For each={tweet().extended_entities?.media}>
             {(item) => (
               <Show when={item.type === "photo"} fallback={<b>{item.type}</b>}>
-                <img src={`${item.media_url}:small`} />
+                <img
+                  src={`${item.media_url}:small`}
+                  width={item.sizes.small.w / devicePixelRatio}
+                  height={item.sizes.small.h / devicePixelRatio}
+                />
               </Show>
             )}
           </For>
