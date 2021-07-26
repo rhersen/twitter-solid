2a0b052e5f28d90dd992812e910a205484be735f
Rudolf Hersén <rudolf.hersen@schibsted.com>
2021-07-26 08:57:31 +0200

    display: grid

--- src/Tweet.jsx
+++ src/Tweet.jsx
@@ -4,64 +4,58 @@ export default function Tweet(props) {
   const tweet = () =>
     props.tweet.retweeted_status ? props.tweet.retweeted_status : props.tweet;
 
-  const fullText = () => tweet().full_text.split("\n");
+  const fullText = () =>
+    tweet()
+      .full_text.split("\n")
+      .filter((x) => x);
 
   return (
-    <>
-      <div class="stats">
-        <button
-          class="countdown"
-          onClick={async () => {
-            console.log("PUT", props.tweet.id_str);
-            console.log(
-              (
-                await fetch("/.netlify/functions/fauna", {
-                  method: "PUT",
-                  body: props.tweet.id_str,
-                })
-              ).status
-            );
-          }}
-        >
-          mark
-        </button>
-        <hr />
-      </div>
-      <li>
-        {props.tweet.created_at.substr(8, 8)}{" "}
-        <Show when={props.tweet.retweeted_status}>
-          <i>{props.tweet.user.screen_name}</i>
-        </Show>{" "}
-        <b>{tweet().user.screen_name}</b>
-        <For each={fullText()}>{(p) => <p>{p}</p>}</For>
-        <ol>
-          <For each={tweet().entities?.urls}>
-            {(url) => (
-              <li>
-                <a href={url.url} target="_blank">
-                  {url.display_url}
-                </a>
-              </li>
-            )}
-          </For>
-        </ol>
-        <Show when={tweet().quoted_status}>
-          <div class="quoted">{tweet().quoted_status.full_text}</div>
-        </Show>
-        <ul>
-          <For each={tweet().extended_entities?.media}>
-            {(item) => (
-              <Show when={item.type === "photo"} fallback={<b>{item.type}</b>}>
-                <img
-                  src={`${item.media_url}:small`}
-                  width={item.sizes.small.w / devicePixelRatio}
-                  height={item.sizes.small.h / devicePixelRatio}
-                />
-              </Show>
-            )}
-          </For>
-        </ul>
-      </li>
-    </>
+    <div class="tweet">
+      <button
+        class="countdown"
+        onClick={async () => {
+          console.log("PUT", props.tweet.id_str);
+          console.log(
+            (
+              await fetch("/.netlify/functions/fauna", {
+                method: "PUT",
+                body: props.tweet.id_str,
+              })
+            ).status
+          );
+        }}
+      >
+        mark
+      </button>
+      <div class="created-at">{props.tweet.created_at.substr(8, 8)}</div>
+      <b>{tweet().user.screen_name}</b>
+      <Show when={props.tweet.retweeted_status}>
+        <i>{props.tweet.user.screen_name}</i>
+      </Show>
+      <For each={fullText()}>{(p) => <div>{p}</div>}</For>
+      <For each={tweet().entities?.urls}>
+        {(url) => (
+          <div class="url">
+            <a href={url.url} target="_blank">
+              {url.display_url}
+            </a>
+          </div>
+        )}
+      </For>
+      <Show when={tweet().quoted_status}>
+        <div class="quoted">{tweet().quoted_status.full_text}</div>
+      </Show>
+      <For each={tweet().extended_entities?.media}>
+        {(item) => (
+          <Show when={item.type === "photo"} fallback={<b>{item.type}</b>}>
+            <img
+              src={`${item.media_url}:small`}
+              width={item.sizes.small.w / devicePixelRatio}
+              height={item.sizes.small.h / devicePixelRatio}
+            />
+          </Show>
+        )}
+      </For>
+    </div>
   );
 }
