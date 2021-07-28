514277cc9dc8fcdb3808a8051305ccfe229de8e6
Rudolf Hersén <rudolf.hersen@schibsted.com>
2021-07-28 22:23:39 +0200

    use async/await in twitter.js

--- functions/twitter.js
+++ functions/twitter.js
@@ -5,24 +5,27 @@ const client = new Twitter({
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
   access_token_key: process.env.TWITTER_TOKEN_KEY,
-  access_token_secret: process.env.TWITTER_TOKEN_SECRET
+  access_token_secret: process.env.TWITTER_TOKEN_SECRET,
 });
 
-exports.handler = function ({ queryStringParameters }, context, callback) {
-  client.get(
-    "statuses/home_timeline",
-    {
+exports.handler = async function (
+  { queryStringParameters },
+  context,
+  callback
+) {
+  try {
+    const data = await client.get("statuses/home_timeline", {
       tweet_mode: "extended",
       exclude_replies: "true",
       include_rts: "true",
       since_id: queryStringParameters.since_id,
-      count: "200"
-    },
-    (error, data) => {
-      callback(error && error.map((e) => e.message), {
-        statusCode: 200,
-        body: JSON.stringify(data)
-      });
-    }
-  );
+      count: "200",
+    });
+    callback(undefined, {
+      statusCode: 200,
+      body: JSON.stringify(data),
+    });
+  } catch (error) {
+    callback(error && error.map((e) => e.message));
+  }
 };
